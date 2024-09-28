64309818d001864de2d28f734a2c2ccfa9ef1a39
CodeMasterYoutube <bmmm1370@gmail.com>
2024-09-28 14:59:58 -0400

    BST insertion

---
+++ BST_insert.py
@@ -0,0 +1,58 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+# A utility function to insert
+# a new node with the given key
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    if root.val == key:
+            return root
+    if root.val < key:
+            root.right = insert(root.right, key)
+    else:
+            root.left = insert(root.left, key)
+    return root
+
+
+# A utility function to do inorder tree traversal
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val, end=" ")
+        inorder(root.right)
+def level(root):
+    if not root:
+        return
+    q = [root]
+    result = []
+    while q:
+        node = q.pop(0)
+        result.append(node.val)
+        if node.left: q.append(node.left)
+        if node.right: q.append(node.right)
+    print(result)
+
+
+# Creating the following BST
+#      50
+#     /  \
+#    30   70
+#   / \   / \
+#  20 40 60 80
+r = Node(50)
+r = insert(r, 20)
+r = insert(r, 30)
+r = insert(r, 40)
+r = insert(r, 70)
+r = insert(r, 60)
+r = insert(r, 80)
+
+# Print inorder traversal of the BST
+inorder(r)
+print("/")
+level(r)
